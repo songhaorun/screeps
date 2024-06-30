fe2e0f40f85615c3e29037ecee886cf82a39b31c
songhaorun <1963535597@qq.com>
2024-07-01 02:34:00 +0800

    将respawn.js移动到主文件夹 将creep的运行相关代码放到单独一个文件中

---
+++ src/creeps.js
@@ -0,0 +1,34 @@
+const roleHarvester = require('role_harvester');
+const roleUpgrader = require('role_upgrader');
+const roleBuilder = require('role_builder');
+const roleCarrier = require('role_carrier');
+const roleRepairer = require('role_repairer');
+const roleDefender = require('role_defender');
+function creeps_run(){
+    for(const name in Game.creeps) {
+        const creep = Game.creeps[name];
+        switch(creep.memory.role){
+            case 'harvester':
+                roleHarvester.run(creep);
+                break;
+            case 'upgrader':
+                roleUpgrader.run(creep);
+                break;
+            case 'builder':
+                roleBuilder.run(creep);
+                break;
+            case 'carrier':
+                roleCarrier.run(creep);
+                break;
+            case 'repairer':
+                roleRepairer.run(creep);
+                break;
+            case 'defender':
+                roleDefender.run(creep);
+                break;
+            default:
+                console.log('Error:'+name+'has undefined role!');
+        }
+    }
+}
+module.exports = creeps_run;
--- src/main.js
+++ src/main.js
@@ -1,39 +1,8 @@
-var roleHarvester = require('role_harvester');
-var roleUpgrader = require('role_upgrader');
-var roleBuilder = require('role_builder');
-var roleCarrier = require('role_carrier');
-var roleRepairer = require('role_repairer');
-var roleDefender = require('role_defender');
-var respawn = require('tools_respawn');
-var tower_run = require('tower');
-
+const respawn = require('respawn');
+const tower_run = require('tower');
+const creeps_run = require('creeps');
 module.exports.loop = function () {
-
     respawn();
     tower_run();
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        switch(creep.memory.role){
-            case 'harvester':
-                roleHarvester.run(creep);
-                break;
-            case 'upgrader':
-                roleUpgrader.run(creep);
-                break;
-            case 'builder':
-                roleBuilder.run(creep);
-                break;
-            case 'carrier':
-                roleCarrier.run(creep);
-                break;
-            case 'repairer':
-                roleRepairer.run(creep);
-                break;
-            case 'defender':
-                roleDefender.run(creep);
-                break;
-            default:
-                console.log('Error:'+name+'has undefined role!');
-        }
-    }
+    creeps_run();
 }
